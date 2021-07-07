528e34a88add1bac8221ecc48e241e0525f0cbde
ptrcxy <caoxuyuan@outlook.com>
2021-07-07 22:51:08 +0800

    Initial commit

---
+++ src/main/java/com/ptr/alioss/AliOssApplication.java
@@ -0,0 +1,13 @@
+package com.ptr.alioss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliOssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptr/alioss/AliOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptr.alioss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
